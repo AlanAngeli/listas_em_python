631a79237de39fb9605e7383a369b02322f59710
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-02-02 19:28:33 -0300

    Add files via upload

---
+++ listas_em_python.py
@@ -0,0 +1,23 @@
+"""
+
+append, insert, pop, del, clear, extend, +
+min, max
+range
+
+"""
+#índice:  0   1   2   3   4
+lista = ["A","B","C","D","E"]
+#índice - 4   3   2   1   0
+
+string = "ABCDE"
+
+print(lista[3])
+print()
+
+lista2 = ["Amora","Banana","Coco","Maçã","Abacaxi"]
+print(lista2[3])
+print()
+
+lista2 = ["Amora","Banana","Coco","Maçã","Abacaxi",10.5]
+print(lista2[-1]) # ou print(lista2[5])
+print()
